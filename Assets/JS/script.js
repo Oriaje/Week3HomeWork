bd822b37ed5d9c421324183da4dbc4bd58e8a875
Oriaje <mthrice@hotmail.com>
2022-01-03 15:26:07 +1000

    setting the password length parameters

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword () {
+let passwordlength;
+//get password length
+passwordLength = parseInt(prompt("Please specify a password length atleast 8 to 128 characters"))
+if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+  alert("Invalid password length entered")
+  return ""
+  //using parseInt to change string to value, as prompt returns a string
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
