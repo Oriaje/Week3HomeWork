2458c5a933d42a8c09eb40e1b317c43c3b5deec5
Oriaje <mthrice@hotmail.com>
2022-01-03 20:43:22 +1000

    settingt the password criteria

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,16 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword () {
-let passwordlength;
-//get password length
-passwordLength = parseInt(prompt("Please specify a password length atleast 8 to 128 characters"))
-if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-  alert("Invalid password length entered")
-  return ""
+function generatePassword() {
+  let passwordlength;
+  //get password length
   //using parseInt to change string to value, as prompt returns a string
-}
+  passwordLength = parseInt(
+    prompt("Please specify a password length atleast 8 to 128 characters")
+  );
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Invalid password length entered");
+    //using the next code line to remove the word undefined
+    return "";
+  }
+  // setting the criteria for password
 
+  includeLowerCase = confirm("Do you want to include lower case characters");
+
+  includeUpperCase = confirm("Do you want to include upper case characters");
+
+  includeNumeric = confirm("Do you want to include numeric characters");
+
+  includeSpecialCharacters = confirm(
+    "Do you want to include special characters"
+  );
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -18,7 +32,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
